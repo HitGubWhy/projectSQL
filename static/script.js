81502fdbaab2cdb89f6ec0745dc2bad33f1a0721
HitGubWhy <daniil.borisov.pc@gmail.com>
2025-04-22 21:44:19 +0300

    update

---
+++ static/script.js
@@ -0,0 +1,34 @@
+const socket = io()
+
+const messages = document.getElementById("messages")
+const form = document.getElementById("form")
+const input = document.getElementById("input")
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    if (input.value) {
+        socket.emit('new_message', input.value)
+        input.value = ''
+    }
+})
+
+socket.on('message', function(msg){
+    console.log(msg)
+    var item = document.createElement('li')
+    item.textContent = msg
+    messages.appendChild(item)
+    window.scrollTo(0, document.body.scrollHeight)
+})
+
+function changeNick() {
+    let nickname = prompt('Choose your nickname!')
+    if (nickname) {
+        socket.emit('set_nick', nickname)
+    }
+}
+
+changeNick()
+
+
+
+
